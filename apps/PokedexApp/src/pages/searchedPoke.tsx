1296395f87d682e84a5c9d9c365d246d265bff0e
PatsakornW <patsakorn.wtnsb@gmail.com>
2023-09-23 18:54:06 +0700

    Add_Poker_and_Pokedex

---
+++ apps/PokedexApp/src/pages/searchedPoke.tsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { PokeContext } from "../context/pokeContext";
+
+
+
+
+export const SearchedPoke = () => {
+
+
+
+  return (
+    <div>
+      <p>aaa</p>
+    </div>
+  )
+}
